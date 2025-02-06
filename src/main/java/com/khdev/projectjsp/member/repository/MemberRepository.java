e14cbae8a37d3f6a03a5b5ae1084a87f51ae9396
KYOOHYUN <khyhlee@naver.com>
2025-02-06 13:45:53 +0900

    connect to Database

--- src/main/java/com/khdev/projectjsp/member/repository/MemberRepository.java
+++ src/main/java/com/khdev/projectjsp/member/repository/MemberRepository.java
@@ -1,15 +1,23 @@
 package com.khdev.projectjsp.member.repository;
 
 import com.khdev.projectjsp.member.dto.MemberDTO;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
+    private final SqlSessionTemplate sql;
+
     public int save(MemberDTO memberDTO) {
         /*
         jsp에서 입력한 것들이 여기까지 잘와서 찍히나 보려고.
         */
         System.out.println("memberDTO = " + memberDTO);
-        return 0;
+        return sql.insert("Member.save", memberDTO);
+        //memberMapper.xml에 insert id = "save"
+
     }
 }
